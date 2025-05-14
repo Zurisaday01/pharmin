88491e5d8eac4fc8c67b0997546c5dde42fcc116
Zury01 <Zurisaday01@hotmail.com>
2025-05-13 18:21:02 -0600

    Fixed auth error type

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,5 +1,9 @@
 import { AuthError } from 'next-auth';
 
+interface AError extends AuthError {
+	type?: string;
+}
+
 // Format errors
 export function formatError(error: any) {
 	if (error.name === 'ZodError') {
@@ -16,7 +20,7 @@ export function formatError(error: any) {
 		// Handle Prisma error
 		const field = error.meta?.target ? error.meta.target[0] : 'Field';
 		return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
-	} else if ((error as AuthError)?.type === 'CredentialsSignin') {
+	} else if ((error as AError)?.type === 'CredentialsSignin') {
 		// Handle credentials error
 		return 'Invalid credentials';
 	} else {
